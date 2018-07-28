1ec0b9261407493edcafca5d4d93c4aedeec77d4
Jeff Grossman <jeff@jeffgrossman.me>
2018-07-28 15:40:16 -0400

    Initial Commit

---
+++ BottomDrawerTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BottomDrawerTest
+//
+//  Created by Jeff Grossman on 7/28/18.
+//  Copyright © 2018 Jeff Grossman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
